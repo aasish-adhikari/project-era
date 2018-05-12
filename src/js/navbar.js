1ade1d22eab63566c67d008e7bc678a00b6d251f
Aasish Adhikari <gemini.aasis@gmail.com>
2018-05-12 12:57:41 +0930

    sidebar nav js uncommented

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,11 +1,11 @@
-/*document.getElementsByClassName('sidenav-trigger')[0].addEventListener('click', showMenu);
+document.getElementsByClassName('sidenav-trigger')[0].addEventListener('click', showMenu);
 window.addEventListener('scroll',scrolling());
 function showMenu(){
     var expand = document.getElementsByClassName("sidenav")[0];
     console.log('noth');
         expand.classList.toggle("show");
 }
-
+/*
 function scrolling(){
   var nav = document.getElementsByClass('mainnav')[0];
   //var body  = document.getElementsByClassName("container")[0];
