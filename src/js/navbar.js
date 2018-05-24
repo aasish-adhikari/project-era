e21fab52e58ff3b778d974cc403c5031a663050c
Aasish Adhikari <gemini.aasis@gmail.com>
2018-05-25 00:34:29 +0930

    styling added

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,16 +1,18 @@
 document.getElementsByClassName('sidenav-trigger')[0].addEventListener('click', showMenu);
-// window.addEventListener('scroll',scrolling());
 function showMenu(){
     var expand = document.getElementsByClassName("sidenav")[0];
     console.log('noth');
         expand.classList.toggle("show");
 }
-/*
-function scrolling(){
-  var nav = document.getElementsByClass('mainnav')[0];
+
+window.addEventListener('scroll',function(e){
+  var nav = document.getElementsByClassName("nav-home")[0];
   //var body  = document.getElementsByClassName("container")[0];
-  if(document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight-45){
+  if((document.documentElement.scrollTop || document.body.scrollTop) > window.innerHeight){
     nav.classList.add('change');
   }
+  else{
+    nav.classList.remove('change');
+  }
 
-}*/
+});
